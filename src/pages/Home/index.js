a13957eec8d2f2506f006f0b7f6ac4a9ef2c8331
Carlos Filho <carlos.filho@sinqia.com.br>
2020-04-04 16:47:04 -0300

    Commit Inicial

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import './styles.css';
+
+
+export default function Home(){
+    return(
+        <header>
+            
+        </header>
+    );
+}
